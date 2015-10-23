ed0ae26321c4d18bfa26c906e158b62d83ff1aed
sthzg <sthzg@gmx.net>
2015-10-23 23:05:33 +0200

    Initial alpha_dev commit

---
+++ fbstore/index.js
@@ -0,0 +1,12 @@
+'use strict';
+let generator = require('yeoman-generator');
+
+module.exports = generator.NamedBase.extend({
+
+  constructor: function() {
+    generator.NamedBase.apply(this, arguments);
+  },
+
+  writing: function() {
+  }
+});
